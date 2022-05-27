8f9f45f1e79baedabb491985010aa881050de192
Lucas Fidalgo <lucasfidalgo07@gmail.com>
2022-05-27 13:19:37 -0300

    .

---
+++ 2_fundamentos/string.js
@@ -0,0 +1,12 @@
+const alimento = 'Aipim'
+
+console.log(alimento.charAt(4))
+console.log(alimento.charCodeAt(2))
+
+console.log(alimento.substring(1))
+console.log(alimento.substring(0, 4))
+
+console.log(alimento.concat(' Frito'.concat('!!')))
+console.log(alimento.replace('i', 'e'))
+
+console.log('Jonas,Dedeh,Matheus,Lucas'.split(','))
