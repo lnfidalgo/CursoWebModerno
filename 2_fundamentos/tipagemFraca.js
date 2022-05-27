53237db82aecc7668721b60871f9c7e9e6f1b1f3
Lucas Fidalgo <lucasfidalgo07@gmail.com>
2022-05-26 21:21:48 -0300

    .

---
+++ 2_fundamentos/tipagemFraca.js
@@ -0,0 +1,12 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1213
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// Evitar nome genérico e sigla
+let valor = ''
+let numero = 1
+let pqp = false // Produto Químico Perigoso 
