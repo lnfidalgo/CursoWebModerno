7e43bc4f208f672c7a0a6eda61fa2376d9711065
Lucas Fidalgo <lucasfidalgo07@gmail.com>
2022-05-28 00:27:01 -0300

    .

---
+++ 2_fundamentos/object.js
@@ -0,0 +1,14 @@
+const prod1 = {}
+prod1.nome = 'Celular Bom Demais Da Conta'
+prod1.preco = 5000.90
+
+console.log(prod1)
+
+const prod2 = {
+  nome: 'Aipim Frito',
+  preco: 80.01
+}
+
+'{ "nome": "Aipim Frito", "preco": 80.00 }' // isso é um JSON
+
+console.log(prod2)
