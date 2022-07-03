84e94df0510f4242be667ee13d574d2aa5770004
Lucas Fidalgo <lucasfidalgo07@gmail.com>
2022-07-03 01:38:14 -0300

    .

--- 4_funcao/exerciciosFuncao/8_basquete.js
+++ 4_funcao/exerciciosFuncao/8_basquete.js
@@ -1,1 +1,23 @@
+let pontuacaoString = '30, 40, 20, 4, 51, 25, 42, 38, 56, 0'
 
+function avaliarPontuacao(pontuacaoString) {
+  let pontuacoes = pontuacaoString.split(', ')
+  let qtdQuebraRecorde = 0
+  let piorJogo = 1
+  let maiorPontuacao = pontuacoes[0]
+  let menorPontuacao = pontuacoes[0]
+
+  for (let i = 1; i < pontuacoes.length; i++) {
+    if(pontuacoes[i] > maiorPontuacao) {
+      maiorPontuacao = pontuacoes[i]
+      qtdQuebraRecorde++
+    } else if (pontuacoes[i] < menorPontuacao) {
+      menorPontuacao = pontuacoes[i]
+      piorJogo = i + 1;
+    }
+  }
+
+  return [qtdQuebraRecorde, piorJogo]
+}
+
+console.log(avaliarPontuacao(pontuacaoString))
