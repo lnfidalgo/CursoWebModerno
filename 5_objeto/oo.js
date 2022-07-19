69dc591838e4478ecbaf4c8de4b0377eabf1a2f7
Lucas Fidalgo <lucasfidalgo07@gmail.com>
2022-07-19 00:13:43 -0300

    .

---
+++ 5_objeto/oo.js
@@ -0,0 +1,22 @@
+//  CÓDIGO NÃO EXECUTÁVEL
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+  // OO
+  objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+      // ...
+    }
+  }
+
+  objeto.processamento() // Foco passou a ser o objeto
+
+  // Principios importantes:
+  // 1. Abstração : Pegar um objeto da vida real e traduzir para o seu código(sistema)
+  // 2. Encapsulamento: Detalhes de implementação escondidos, e você mostrar para o cliente(usuário do sistema), a interface simples para interagir com determinado objeto
+  // 3. Herança: (prototype) priorize a composição em vez da herança
+  // 4. Polimorfismo:
